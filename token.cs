5a58740f7f9ee62e65ac684937c6f529741092eb
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2024-06-21 20:45:20 +0200

    token acctualy work

--- token.cs
+++ token.cs
@@ -1,9 +1,32 @@
-using system;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace cfun{
-  class Token{
-    public string name;
-    public string val;
+namespace CfunLang.CfunLang
+{
+    public class Token
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
 
-  }
+        public Token(string type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            if (Value == "")
+            {
+                return $"Token(Type: {Type})";
+            }
+            else
+            {
+                return $"Token(Type: {Type}, Value: {Value})";
+            }
+        }
+    }
 }
